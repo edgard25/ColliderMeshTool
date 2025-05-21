82066587be0fe44fe71485623c5bdc5f80d0d1dd
SinlessDevil <83553523+SinlessDevil@users.noreply.github.com>
2025-05-21 16:01:46 +0300

    Initial commit

---
+++ Assets/Code/Infrastructure/Services/SaveLoad/PrefsSaveLoadService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Code.Infrastructure.Services.PersistenceProgress;
+using Code.Infrastructure.Services.PersistenceProgress.Player;
+using Sirenix.Serialization;
+using UnityEngine;
+
+namespace Code.Infrastructure.Services.SaveLoad
+{
+    public class PrefsSaveLoadService : ISaveLoadService
+    {
+        private const string PlayerDataKey = "PlayerData";
+        
+        private readonly IPersistenceProgressService _progressService;
+
+        public PrefsSaveLoadService(IPersistenceProgressService progressService)
+        {
+            _progressService = progressService;
+        }
+        
+        public void SaveProgress() => Save(_progressService.PlayerData);
+        
+        public void Save(PlayerData playerData)
+        {
+            byte[] serializedValue = SerializationUtility.SerializeValue(playerData, DataFormat.JSON);
+            string base64String = Convert.ToBase64String(serializedValue);
+            PlayerPrefs.SetString(key: PlayerDataKey, base64String);
+            PlayerPrefs.Save();
+            Debug.Log($"💾 PlayerData saved to PlayerPrefs: {PlayerDataKey}");
+        }
+
+        public PlayerData Load()
+        {
+            string base64String = PlayerPrefs.GetString(key: PlayerDataKey, string.Empty);
+
+            if (base64String == string.Empty)
+                return null;
+
+            byte[] serializedValue = Convert.FromBase64String(base64String);
+            return SerializationUtility.DeserializeValue<PlayerData>(serializedValue, DataFormat.JSON);
+        }
+    }
+}
---
+++ Assets/Code/Infrastructure/StaticData/BalanceStaticData.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace StaticData
+{
+    [CreateAssetMenu(menuName = "StaticData/Balance", fileName = "Balance", order = 0)]
+    public class BalanceStaticData : ScriptableObject
+    {
+        
+    }
+}
